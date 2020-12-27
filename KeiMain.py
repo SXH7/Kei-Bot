03141138a25d4346b4a362b15df7b4e28aba04b7
Shashwat Singh <70637141+shashwat-kun@users.noreply.github.com>
2020-12-27 15:22:24 +0530

    Add files via upload
    Main Code of Kei Bot

---
+++ KeiMain.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = 'k!', intents = intents)
+
+@client.event
+async def on_ready():
+    print("The bot is ready.")
+
+client.run('NzkyNjExOTU3MDM0OTc1MjMy.X-gPaA.GPVTjMgw-jXmr2i8Tbf2RSozWiY')
