b5a01cb70f78bca3250c4e6dac550121805e0b2f
Shashwat Singh <70637141+shashwat-kun@users.noreply.github.com>
2020-12-27 16:01:54 +0530

    Update KeiMain.py

--- KeiMain.py
+++ KeiMain.py
@@ -1,11 +1,18 @@
 import discord
 from discord.ext import commands
 
+file = open('token.txt', 'r')
+data = file.read()
+
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = 'k!', intents = intents)
 
 @client.event
 async def on_ready():
-    print("The bot is ready")
+    print("The bot is ready.")
 
-client.run('NzkyNjExOTU3MDM0OTc1MjMy.X-gPaA.GPVTjMgw-jXmr2i8Tbf2RSozWiY')
+@client.command()
+async def load(ctx, extention):
+    client.load_extension()
+
+client.run(data)
