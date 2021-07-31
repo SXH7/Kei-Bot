d6093afc61b5135f8e315aa2e6be8c2c435120f1
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2021-07-31 19:57:32 +0530

    adding anilist commands
    this is not fully functional yet, the code in incomplete

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -2,6 +2,16 @@ import discord
 from discord.ext import commands
 import praw
 import random
+import anilist
+
+
+reddit = praw.Reddit(client_id = "tpThwIQjP1tYhA",
+                     client_secret = "iiz8VruzO5y2XRksqClXxQxC6hXCIQ",
+                     username = "shashwat_senpai",
+                     password = "shashwwat@reddit",
+                     user_agent = "Kei Bot")
+
+defaultUserUrl = 'https://anilist.co/user/'
 
 class FunCommands(commands.Cog):
     def __init__(self, client):
@@ -13,6 +23,8 @@ class FunCommands(commands.Cog):
 
     @commands.command()
     async def reddit(self, ctx, sub: str = ""):
+        '''if(sub == "hentai" or sub == "porn" or sub == "fiftyfifty" or sub == "Hentai" or sub == "Porn"):
+            await ctx.send(file=discord.File('bonk.png'))'''
         subreddit = reddit.subreddit(sub)
         hot = subreddit.hot(limit = 100)
         allsub = []
@@ -25,8 +37,25 @@ class FunCommands(commands.Cog):
 
         em = discord.Embed(title = name)
         em.set_image(url = url)
+        if post.over_18:
+            await ctx.send("https://imgur.com/a/5HKgTzU")
+        if(sub != "hentai" and sub != "porn" and sub != "fiftyfifty"):
+            await ctx.send(embed = em)
 
-        await ctx.send(embed = em)
+
+
+"""    @commands.group(name='anilist', invoke_without_command=True)
+    async def anilist(self, ctx):
+        await ctx.channel.send('The anilist commands available are: **user**, **anime**, **manga**.')
+        
+    @anilist.command(name='user')
+    async def user_subcommand(self, ctx, *, username):
+        await ctx.channel.send(defaultUserUrl+username)
+
+    @anilist.command(name='anime')
+    async def anime_subcommand(self, ctx):
+        await ctx.channel.send('anime subcommand')"""
+
 
 def setup(client):
     client.add_cog(FunCommands(client))
