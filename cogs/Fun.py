de0698242c3d1486d361b89a942bc0e920f8f5a2
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2021-08-26 10:15:20 +0530

    added shrex command

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,7 +1,9 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+#from discord.utils import get
 import praw
 import random
+from PIL import Image, ImageDraw, ImageFont
 #import anilist
 
 
@@ -23,6 +25,8 @@ class FunCommands(commands.Cog):
 
     @commands.command()
     async def reddit(self, ctx, sub: str = ""):
+        '''if(sub == "hentai" or sub == "porn" or sub == "fiftyfifty" or sub == "Hentai" or sub == "Porn"):
+            await ctx.send(file=discord.File('bonk.png'))'''
         subreddit = reddit.subreddit(sub)
         hot = subreddit.hot(limit = 100)
         allsub = []
@@ -39,6 +43,44 @@ class FunCommands(commands.Cog):
             await ctx.send("https://imgur.com/a/5HKgTzU")
         else:
             await ctx.send(embed = em)
+    @commands.command()
+    async def shrex(self, ctx, *,user=None):
+        if(user != None):
+            if(ctx.message.mentions):
+
+                user = user.replace("<", "")
+                user = user.replace(">", "")
+                user = user.replace("@", "")
+                user = user.replace("!", "")
+
+                username = str(ctx.guild.get_member(int(user)))
+                length = len(username)
+                username = username[:length-5]
+
+                img = Image.open('C:/Users/shash/PycharmProjects/Kei-Bot/cogs/resources/shrex.jpg')
+                draw = ImageDraw.Draw(img)
+                author = ctx.author.name
+                author = author.lower()
+                font = ImageFont.truetype('C:/Users/shash/PycharmProjects/Kei-Bot/cogs/resources/comic.ttf', 14)
+                draw.text((20, 194), username, (255, 255, 255), font=font, stroke_width=1, stroke_fill='#000000')
+                draw.text((33, 207), author, (255, 255, 255), font=font, stroke_width=1, stroke_fill='#000000')
+                img.save('img.png')
+                file = discord.File('img.png')
+                await ctx.channel.send(file=file)
+
+            else:
+                img = Image.open('C:/Users/shash/PycharmProjects/Kei-Bot/cogs/resources/shrex.jpg')
+                draw = ImageDraw.Draw(img)
+                author = ctx.author.name
+                author = author.lower()
+                font = ImageFont.truetype('C:/Users/shash/PycharmProjects/Kei-Bot/cogs/resources/comic.ttf', 14)
+                draw.text((20, 194), user, (255, 255, 255), font=font, stroke_width=1, stroke_fill='#000000')
+                draw.text((33, 207), author, (255, 255, 255), font=font, stroke_width=1, stroke_fill='#000000')
+                img.save('img.png')
+                file = discord.File('img.png')
+                await ctx.channel.send(file=file)
+        if(user==None):
+            await ctx.channel.send("You must pass something with the command.")
 
 
 
